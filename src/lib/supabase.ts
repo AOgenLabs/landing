7eed98ffc3779e2fc6e6d7648def3dc173befd88
VanshSahay <vansh.sahay1234@gmail.com>
2025-06-12 19:43:32 +0530

    fix: hardcode

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!) 
+const supabaseUrl = 'https://flcinuiljorceyybsszl.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsY2ludWlsam9yY2V5eWJzc3psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MzM3NjEsImV4cCI6MjA2NTMwOTc2MX0.l7LJEq8-j-PCEbSgsgbGqSprFIxFGyAzBHgeEmkkTTc'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
